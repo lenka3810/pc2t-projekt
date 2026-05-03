8d28b36383a97cd31cd8b70796d2773dec290984
sajmujel <samuel.bachraty1@gmail.com>
2026-05-03 18:29:03 +0200

    Implement risk score calculation in pouzitZrucnost method

--- pc2t projekt/src/BezpecnostnySpecialista.java
+++ pc2t projekt/src/BezpecnostnySpecialista.java
@@ -12,8 +12,23 @@ public class BezpecnostnySpecialista extends Zamestnanec {
 	
 	@Override
 	public void pouzitZrucnost() {
-		
+			if(spoluprace.isEmpty()) {
+				System.out.println(this+" nema ziadnych spolupracovnikov.");
+				return;
+			}
+			
+			int pocet = spoluprace.size();
+			double sucet = 0.0;
+			
+			for (UrovenSpoluprace u : spoluprace.values()) {
+				if (u == UrovenSpoluprace.DOBRA) sucet += 1;
+				else if (u == UrovenSpoluprace.PRIEMERNA) sucet += 2;
+				else sucet += 3;
+			}
+			
+			double priemer = sucet/pocet;
+			double skore = pocet*priemer;
+			
+			System.out.println(this+" rizikove skore: "+ String.format("%.2f", skore));
 	}
-
-
 }
